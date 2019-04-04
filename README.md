1b87a6b8295046c36394cf3108ae8a9ac65062ee
Vince216 <vinceshury@gmail.com>
2019-04-04 13:29:54 -0400

    finalizing readme after code has been cleaned up alot

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # liri-node-app
 
-   * LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives back data.
+   * LIRI is a _Language Interpretation and Recognition Interface_. LIRI is designed to be a command line node app that takes in parameters and displays data to the console.
 
    * LIRI will search:
 
@@ -12,11 +12,96 @@
 
    * Using Axios to make the calls to Bands In Town API & OMDB API
 
-   * Utilizing Inquirer to take in user input for addional searches & Allow more then one command to be ran each execution of the application. 
+   * Using the node-spotify-api npm package to make the spotify API calls 
+
+   * Utilizing Inquirer npm  to take in user input for addional searches & Allow more then one command to be ran each execution of the application. 
 
 ---
 
 ## This application Allows the user to search 3 different API's depending on the selected or Inputted command. 
 
+The liri command line node application allows you to search three separate APIs quickly in one application to display Data, using keywords for the commands to determine which API to search then the app will display the results of that API's response to the console after api request is done. LIRI will then prompt the user to see if they would like to search again. 
+
+      If they select NO the liri.js process will terminate 
+
+      If they select YES
+      liri will then display a list of the three possible commands to search from before allowing the user to input a new search after the command is selected.
+
+ _If any of the searches an error occurs the error data will display and prompt the user to select Yes or no to if they would like to ask another question, default: No, closes the app. Answering Yes allows you to search again until you select no to the Search again prompt._ 
+
+### How the liri.js App works
+
+   * node liri spotify-this-song < song name > [Spotify API Track Search Endpoint](https://developer.spotify.com/console/get-search-item/)
+
+   * node liri movie-this < movie name > [OMDB API](http://www.omdbapi.com)
+
+   * node liri concert-this < artist or band > [Bands In Town API](http://www.artists.bandsintown.com/bandsintown-api)
+
+   * node liri do-what-it-says     _(will read from the random.txt file.)_
+
+   *The LIRI application utilizes and requires these npm packages:*
+
+  * [Moment](https://www.npmjs.com/package/moment)
+
+  * [DotEnv](https://www.npmjs.com/package/dotenv)
+
+  * [Axios](https://www.npmjs.com/package/axios)
+
+  * [inquirer](https://www.npmjs.com/package/inquirer)
+
+  * [node-spotify-api](https://www.npmjs.com/package/node-spotify-api)
+
+---
+
+## Getting Started with the LIRI node command line application
+
+_**WARNING: If you are not familiar with the bash/terminal this app may not be suitible for you but feel free to try anyways**_.
+
+Welcome user! So you want to use my command line node app? Okay! lets get started.
+ You are going to need to have [Node.js](https://nodejs.org/en/) Installed in order to run this javascript file in the terminal or command line. Assuming you are familiar with node as well as the terminal/cli and are ready to move on continue to the next steps 
 
 
+ * STEP ONE: You will need to go to my [Github](https://github.com/Vincent440/liri-node-app) repository, you can then either fork the repository and copy the Clone with HTTPS link and run the following command from your terminal or bash window or clone my repository the same way using the link on my Github repo the command will look like this in the terminal:
+
+```
+git clone https://github.com/Vincent440/liri-node-app.git
+```
+ Running that command on your computers terminal or command line will download the repository to your computers local drive. and you should see the following :
+
+ [image of git clone screen shot will go here]
+
+
+* STEP TWO: You will have to run npm install to download the required npm packages in order for the application to function. that command will look like:
+
+```
+npm install
+npm i
+```
+After running that command in the terminal you should have all the required packages installed a package-lock.json file created with a node modules directory made and populated with the required files.
+
+[image of node packages successfully completed install goes here]
+
+* STEP THREE: You are going to need to create a .env file in the same directory (this is where your Spotify api ID & Secret will go later). After you have all the files created you will need to make sure you have an API Key for each of the API's. Two are included in the REPO you will however need a Spotify Key Visit [Spotifys Developer page here](https://developer.spotify.com/my-applications/#!/applications/create) to get one. Your spotify api key will go in the **.env** file which will look like : 
+
+```
+# Spotify API keys
+
+SPOTIFY_ID=YOUR SPOTIFY API ID WILL NEED TO BE STORED HERE
+SPOTIFY_SECRET=YOUR SPOTIFY SECRET WILL NEED TO BE STORED HERE 
+
+```
+
+Once you have the Spotify ID & Secret in place of the text above after the "=". you will be ready to run the liri.js application in the terminal to use it! Open up your terminal/bash CLI and SEE BELOW for a preview of the application in use to help you get started!
+
+--- 
+
+## Using liri.js & _Example Commands_ 
+
+
+
+```
+node liri concert-this A Day To Remember
+node liri spotify-this-song Wake me up when september ends
+node liri movie-this Guardians of the Galaxy Vol.2
+node liri do-what-it-says
+```
--- liri.js
+++ liri.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-console */
-/* eslint-disable no-undef */
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
@@ -8,24 +6,11 @@ var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
 var inquirer = require('inquirer');
-//=================================================== ✓  GLOBAL VARIABLES ✓  ====================================================
-var command = process.argv[2];
-var inputValue = process.argv;
-var search = "";//get search values + add more then one together with "+'s" 
-var searchLog ="";
-//===============================================================================================================================
-
-for ( var i = 3; i < inputValue.length; i++ ) {//if more then 3 parameters add them together to build search query
-  if (i > 3 && i < inputValue.length) {
-    search = search + "+" + inputValue[i];
-  } else {
-    search += inputValue[i];
-  }
-}
-//===============================================================================================================================
-function liriCall( command , searchInput ) {
-
-  switch (command) {//switch statement to run command into to determine process to run
+var command = process.argv[2];//command starting as the second process 
+var search = process.argv.slice(3).join("+");//uses the input after the command for search parameter with + to fill spaces
+var searchLog="";//global search log variable to take search and remove the + from spaces and display to the user. 
+function liriCall( command , searchInput ) {//switch statement to run command into to determine process to run  
+  switch (command) {
     case "concert-this":
       bandsInTownApi(searchInput);
       break;
@@ -42,81 +27,43 @@ function liriCall( command , searchInput ) {
       searchPromt();
   }
 }
-//===============================================================================================================================
-function bandsInTownApi(bands) {
+function bandsInTownApi(bands) {//bands in town api call, if no input selected calls search prompt 
   if (bands === ""){
-    console.log("\nEnter a band to search for!\n");
-    searchPromt();
-    return;
+   return searchPromt();
   }
-
-  console.log(bands);
   searchLog = bands.replace(/\+/g, " ");
-  console.log(searchLog);
-
-  console.log("=============== SEARCHING Bands In Town API for: " + searchLog + "  ===============");
-
-  //IF STATEMENT TO CATCH IF NO USER INPUT FOR ARTIST TO SEARCH, CALL INQUIRER FUNCTION TO TAKE IN A SEARCH INPUT BEFORE FINISHING THE FUNCTION
-  //---------------------------------- BANDS IN TOWN   ------------ app_id=codingbootcamp
-  axios
-    .get("https://rest.bandsintown.com/artists/" + bands + "/events?app_id=codingbootcamp")
-    .then(//.then for server response
-      function( response ) {//data handling
+  console.log("===============  SEARCHING Bands In Town API for: " + searchLog + "  ===============");
+  axios.get("https://rest.bandsintown.com/artists/" + bands + "/events?app_id=codingbootcamp").then(response => {//data handling
         let events = response.data;
-        for ( let eventIndex = 0 ; eventIndex < events.length; eventIndex++ ) {
-          //Loop through each index of response array
-
-          console.log(
-            "\n================|  SHOWING EVENTS FOR: " +
-              searchLog +
-              " Event #" +
-              eventIndex +
-              "  |=================\n"
-          );
-
+        for ( let eventIndex = 0 ; eventIndex < events.length; eventIndex++ ) {//Loop through entire response array
+          console.log("\n================|  SHOWING EVENTS FOR: " + searchLog 
+          +  " Event #" +  eventIndex +  "  |=================\n");
           console.log("Venue Name: " + events[eventIndex].venue.name); //Name of the venue
-          console.log(
-            "Location: " + events[eventIndex].venue.city + ", " + events[eventIndex].venue.region +
-             " " + events[eventIndex].venue.country ); //Venue location
-          let eventTime = events[eventIndex].datetime;
-          //Date of the Event (use moment to format this as "MM/DD/YYYY")
+          console.log("Location: " + events[eventIndex].venue.city + ", " + events[eventIndex].venue.region 
+          + " " + events[eventIndex].venue.country ); //Venue location
+          let eventTime = events[eventIndex].datetime;//date of event
           console.log("Event Date: " + moment(eventTime).format("MM/DD/YYYY") + "\n");
         }
         searchAgain();
       }
-
-    )
-    .catch(
-      function( error ) {
-        //error handling to .catch errors
-        console.log("==========================|  INTO .CATCH ERROR HANDLING  |============================");
-        console.log(error);
-        console.log("==========================|  END OF .CATCH ERROR HANDLING  |==========================");
-        searchAgain();
+    ).catch(error => {
+        console.log("\nBands In Town Error Data: " + error +"\n\n");
+        return searchAgain();
       }
     );
 }
-//===============================================================================================================================
-function movieDataCall(movieSearch) {
-
+function movieDataCall(movieSearch) {//OMDB Axios api call
   if (movieSearch === "") {
     movieSearch = "Mr+nobody";
   }
   searchLog = movieSearch.replace(/\+/g, " ");
-
   console.log("\n========  SEARCHING OMDB FOR: " + searchLog + "  =======================" );
   axios
-    .get("http://www.omdbapi.com/?t=" + movieSearch + "&apikey=trilogy")
-    .then(
-      //.then for server response
-      function(response) {
-        //data handling
+    .get("http://www.omdbapi.com/?t=" + movieSearch + "&apikey=trilogy")//.then for server response
+    .then(response => {//data handling 
         let movie = response.data;
-        console.log(
-          "\n================  SHOWING SEARCH RESULTS FROM OMDB FOR: " +
-            searchLog +
-            "  =======================\n"
-        );
+        console.log("\n========  SHOWING SEARCH RESULTS FROM OMDB FOR: " +  searchLog +
+         "  =======================\n" );
         console.log("TITLE: " + movie.Title); //TITLE OF MOVIE
         console.log("RELEASED: " + movie.Released); //YEAR OF RELEASE
         console.log("IMDB RATING: " + movie.imdbRating); //IMDB RATING OF MOVIE
@@ -128,71 +75,57 @@ function movieDataCall(movieSearch) {
         console.log("\n==========================================================================\n");
         searchAgain();
       }
-    )
-    .catch(
-      //.catch errors
-      function(error) {
-        //error handling
-        console.log("====================  INTO .CATCH ERROR HANDLING  ==========================");
-        console.log(error);
-        console.log("====================  END OF .CATCH ERROR HANDLING  ========================");
-        searchAgain();
+    ).catch(error => {//error handling display error data and prompt new search
+        console.log("An OMDB error occurred error data: "+error+"\n\n");
+        return searchAgain();
       }
     );
 }
-//===============================================================================================================================
 function spotifyCall(song) {
-
   if (song === "") {//DEFAULT TO < "The Sign" > song by < "Ace of Base" > artist if no input
     song = "The+Sign+Ace+of+Base";
     searchLog = "The Sign, Ace of Base";
   }
   searchLog = song.replace(/\+/g, " ");
-
   console.log("\n=================================   SEARCHING SPOTIFY FOR: " + searchLog + "   ===============================\n");
-  spotify.search({ type: "track", query: song }, function(err, data) {
+  spotify.search({ type: "track", query: song }, (err, data) =>{
     if (err) {
-      return console.log("Error occurred: " + err);
+      console.log("A Spotify error occurred: " + err +"\n\n");
+      return searchAgain();
     } 
     let results = data.tracks.items;
-
     for (let sIndex = 0; sIndex < 5; sIndex++) {// sIndex = Spotify data array index number print 5 songs that match search params.
         console.log( "\n--------------------------  SONG#: " + (sIndex + 1) + " INFO  ----------------------------\n ");
         console.log( "ARTIST NAME:  " + results[sIndex].album.artists[0].name );//ARTIST(s)
         console.log( "\nSONG NAME:  " + results[sIndex].name);//THE SONG'S NAME
         console.log( "\nSPOTIFY TRACK URL:  " + results[sIndex].external_urls.spotify );//PREVIEW LINK OF THE SONG FROM SPOTIFY
         console.log( "\nALBUM NAME:  " + results[sIndex].album.name);//ALBUM NAME THE SONG IS FROM 
-
     }
-
     console.log("\n==================================  END OF " + searchLog + " SPOTIFY SEARCH  =================================================\n");
-    
     searchAgain();
   });
-
 }
-//===============================================================================================================================
 function doWhatItSayss() {
-    fs.readFile("random.txt", "utf8", function(err, data) {
+  console.log("...\n\nReading <command>,<\"search\"> from random.txt file: \n");
+    fs.readFile("random.txt", "utf8", (err, data)=> {
       if (err) {
-        return console.log(err);
+        console.log("Error reading from random.txt file error: "+err);
+        return searchAgain();
+        
       }
-      debugger;
       data = data.split(",");//breaks up the command from the input at the "," comma
       let fileCommand = data[0];
-      let fileSearch = data[1].slice(1).replace(/ /g, "+");//replace the second array index " " with +'s to create a usable search input
+      let fileSearch = data[1].slice(1,-1).replace(/ /g, "+");//replace the second array index " " with +'s to create a usable search input
       liriCall( fileCommand , fileSearch );
     });
 }
-function searchPromt() {//DEFAULT TO THIS, LATER USE INQUIRER FUNCTION TO TAKE IN ANOTHER COMMAND.  
-  //IF THE COMMANDS DON'T MATCH A VALID COMMAND.
-  console.log("\n\n------- Welcome to the LIRI Application please enter a valid [command] and [search input]  ------\n\n");
-  console.log("Valid LIRI Commands are:\n\n node liri < command > < search >\n")
+function searchPromt() {//inquirer search prompt to allow multiple searches and default to if there is no input. 
+  console.log("\n\n\n\n------- Welcome to the LIRI Application please enter a valid [command] and [search input]  ------\n");
+  console.log("Valid LIRI Commands are:\nnode liri < command > < search >\n")
   console.log("concert-this < Artist to search for on Bands In Town >"
     + "\nspotify-this-song < Track to search for on Spotify >\nmovie-this < Movie to search for on OMDB >"+
     "\ndo-what-it-says < executes random.txt command,\"search\" >\n\n"
   );
-
   inquirer
     .prompt([
       {
@@ -204,7 +137,7 @@ function searchPromt() {//DEFAULT TO THIS, LATER USE INQUIRER FUNCTION TO TAKE I
      {
       name: "searchValue",
       message: "Enter what you would like to search for here: ",
-      validate: function(value) {
+      validate: value => {
         if(value.length > 0) {
             return true;
         } else {
@@ -236,12 +169,8 @@ function searchAgain(){
        searchPromt();
      }
      else {
-       console.log("\nThanks for using the LIRI application\n");
-       return;
+      return console.log("\nThanks for using the LIRI application\n");
      }
     });
 }
-
 liriCall(command, search);
-
-//DO BONUS / add functions for inquirer prompt to keep program alive
